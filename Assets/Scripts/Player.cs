84dc2fb00baea09fb9d3caa59cebec044a040ecf
quanglinh01 <120942860+quanglinh01@users.noreply.github.com>
2024-04-24 22:16:22 +0700

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,6 +26,8 @@ public class Player : MonoBehaviour {
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-        Debug.Log(inputVector);
+        
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
